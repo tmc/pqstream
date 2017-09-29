460506c10c232bd69ffeb4aaa9db620042545461
Travis Cline <travis.cline@gmail.com>
2017-09-29 16:37:01 -0700

    patch: cover some happy cases

--- patch_test.go
+++ patch_test.go
@@ -20,6 +20,16 @@ func Test_generatePatch(t *testing.T) {
 		wantErr  bool
 	}{
 		{"nils", args{nil, nil}, "{}", false},
+		{"empties", args{&ptypes_struct.Struct{}, &ptypes_struct.Struct{}}, "{}", false},
+		{"basic", args{&ptypes_struct.Struct{}, &ptypes_struct.Struct{
+			map[string]*ptypes_struct.Value{
+				"foo": &ptypes_struct.Value{
+					Kind: &ptypes_struct.Value_StringValue{
+						StringValue: "bar",
+					},
+				},
+			},
+		}}, `{"foo":"bar"}`, false},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
