987691982dde220afaf195fa38ce570423f788f3
Mark Meyer <mark@ofosos.org>
2017-10-05 09:48:26 +0200

    Add python sample code (#62)
    * Add simple python example code
    
    * Add protoc generated python code to repo
    
    * Add shebang and some useful output

---
+++ clients/python/example.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+import pqstream_pb2_grpc
+import pqstream_pb2
+import grpc
+
+channel = grpc.insecure_channel('localhost:7000')
+stub = pqstream_pb2_grpc.PQStreamStub(channel)
+
+request = pqstream_pb2.ListenRequest()
+request.table_regexp = ".*"
+
+for event in stub.Listen(request):
+  print("Received change, payload follows:")
+  print(event.payload)
+
