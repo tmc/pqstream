089924dc29958136d681b025898a9cfca0186eb3
Mark Meyer <mark@ofosos.org>
2017-10-05 22:01:20 +0200

    Improve testability of the Python example (#64)
    * Improve testability of the Python example
    
    This commit will improve testability of the Python example, by
    creating a run method, that can be tested and calling that from a main
    method, when we run from the command line.
    
    Prep for #63
    
    * Document code and do proper indentation
    
    This example code should now report no errors in the default pylint
    configuration.
    
    Prep for #63

--- clients/python/example.py
+++ clients/python/example.py
@@ -1,16 +1,33 @@
 #!/usr/bin/env python
 
+"""PQstream Python example client
+
+This client connects to a running PQstream instance running at
+localhost on port 7000 and processes, when called from the command
+line, 100 messages and then exits. The payload of the messages will be
+printed to stdout.
+"""
+
 import pqstream_pb2_grpc
 import pqstream_pb2
 import grpc
 
-channel = grpc.insecure_channel('localhost:7000')
-stub = pqstream_pb2_grpc.PQStreamStub(channel)
+def run(messages):
+    """process a set number of messages"""
+    channel = grpc.insecure_channel('localhost:7000')
+    stub = pqstream_pb2_grpc.PQStreamStub(channel)
 
-request = pqstream_pb2.ListenRequest()
-request.table_regexp = ".*"
+    request = pqstream_pb2.ListenRequest()
+    request.table_regexp = ".*"
 
-for event in stub.Listen(request):
-  print("Received change, payload follows:")
-  print(event.payload)
+    i = 0
+    for event in stub.Listen(request):
+        print("Received change, payload follows:")
+        print(event.payload)
+        i = i + 1
+        if i >= messages:
+            return
 
+if __name__ == "__main__":
+    print("Now receiving 100 messages.")
+    run(100)
